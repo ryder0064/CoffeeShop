73abdf39593afc4e6596c26d83cfb7e3b69e4a08
Ryder Chen <ryder.0064@gmail.com>
2024-03-11 21:38:40 +0800

    Using the InMemoryStore in the FakeAuthRepository

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,26 +1,45 @@
 import 'package:coffee_shop/src/features/authentication/domain/app_user.dart';
+import 'package:coffee_shop/src/utils/in_memory_store.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FakeAuthRepository {
-  Stream<AppUser?> authStateChanges() => Stream.value(null); //TODO
-  AppUser? get currentUser => null; //TODO
+  final _authState = InMemoryStore<AppUser?>(null);
+
+  Stream<AppUser?> authStateChanges() => _authState.stream;
+
+  AppUser? get currentUser => _authState.value;
 
   Future<void> signInWithEmailAndPassword(String email, String password) async {
-    //TODO
+    if (currentUser == null) {
+      _createNewUser(email);
+    }
   }
 
   Future<void> createUserWithEmailAndPassword(
       String email, String password) async {
-    //TODO
+    if (currentUser == null) {
+      _createNewUser(email);
+    }
   }
 
   Future<void> signOut() async {
-    //TODO
+    _authState.value = null;
+  }
+
+  void dispose() => _authState.close();
+
+  void _createNewUser(String email) {
+    _authState.value = AppUser(
+      uid: email.split('').reversed.join(),
+      email: email,
+    );
   }
 }
 
 final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
-  return FakeAuthRepository();
+  final auth = FakeAuthRepository();
+  ref.onDispose(() => auth.dispose());
+  return auth;
 });
 
 final authStateChangesProvider = StreamProvider.autoDispose<AppUser?>((ref) {
