e0daab9ea3325b92f10cfdb18cb8c97282945d3c
ryder.chen <ryder.chen@myviewboard.com>
2024-03-18 17:22:00 +0800

    Testing the FakeAuthRepository

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,8 +1,12 @@
 import 'package:coffee_shop/src/features/authentication/domain/app_user.dart';
+import 'package:coffee_shop/src/utils/delay.dart';
 import 'package:coffee_shop/src/utils/in_memory_store.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FakeAuthRepository {
+  FakeAuthRepository({this.addDelay = true});
+
+  final bool addDelay;
   final _authState = InMemoryStore<AppUser?>(null);
 
   Stream<AppUser?> authStateChanges() => _authState.stream;
@@ -10,18 +14,17 @@ class FakeAuthRepository {
   AppUser? get currentUser => _authState.value;
 
   Future<void> signInWithEmailAndPassword(String email, String password) async {
-    await Future.delayed(const Duration(seconds: 3));
+    await delay(addDelay);
     _createNewUser(email);
   }
 
   Future<void> createUserWithEmailAndPassword(
       String email, String password) async {
+    await delay(addDelay);
     _createNewUser(email);
   }
 
   Future<void> signOut() async {
-    // await Future.delayed(const Duration(seconds: 3));
-    // throw Exception('Connection failed');
     _authState.value = null;
   }
 
