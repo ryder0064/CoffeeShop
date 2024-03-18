0aa6f9325c1760a620504d1a5401b7a7114213b8
Ryder Chen <ryder.0064@gmail.com>
2024-03-18 22:45:19 +0800

    Testing with Stream Matchers and Predicates

--- test/src/features/authentication/presentation/account/account_screen_controller_test.dart
+++ test/src/features/authentication/presentation/account/account_screen_controller_test.dart
@@ -17,35 +17,71 @@ void main() {
       expect(controller.state, const AsyncData<void>(null));
     });
 
-    test('signOut success', () async {
-      // setup
-      final authRepository = MockAuthRepository();
-      when(authRepository.signOut).thenAnswer(
-        (_) => Future.value(),
-      );
-      final controller = AccountScreenController(
-        authRepository: authRepository,
-      );
-      // run
-      await controller.signOut();
-      // verify
-      verify(authRepository.signOut).called(1);
-      expect(controller.state, const AsyncData<void>(null));
-    });
-  });
+    /*
+    * Testing each individual state change inside our StateNotifier subclass:
+    * Using expectLater before calling the code under test
+    * Using predicates to write fine-grained expectations
+    * Adding a custom timeout to ensure our tests fail fast when needed
+    * */
+    test(
+      'signOut success',
+      () async {
+        // setup
+        final authRepository = MockAuthRepository();
+        when(authRepository.signOut).thenAnswer(
+          (_) => Future.value(),
+        );
+        final controller = AccountScreenController(
+          authRepository: authRepository,
+        );
 
-  test('signOut failure', () async {
-    // setup
-    final authRepository = MockAuthRepository();
-    final exception = Exception('Connection failed');
-    when(authRepository.signOut).thenThrow(exception);
-    final controller = AccountScreenController(
-      authRepository: authRepository,
+        // Streams emit values over time, meaning that by the time we call expect, all the values have already been emitted, and it's too late to check them.
+        // Instead, we need to start observing our stream before it emits any value.
+        // In other words, we need to expect that our stream will emit some values before we call controller.signOut().
+        expectLater(
+          controller.stream,
+          emitsInOrder(const [
+            AsyncLoading<void>(),
+            AsyncData<void>(null),
+          ]),
+        );
+        // run
+        await controller.signOut();
+        // verify
+        verify(authRepository.signOut).called(1);
+      },
+      timeout: const Timeout(Duration(milliseconds: 500)),
     );
-    // run
-    await controller.signOut();
-    // verify
-    verify(authRepository.signOut).called(1);
-    expect(controller.state.hasError, true);
+    test(
+      'signOut failure',
+      () async {
+        // setup
+        final authRepository = MockAuthRepository();
+        final exception = Exception('Connection failed');
+        when(authRepository.signOut).thenThrow(exception);
+        final controller = AccountScreenController(
+          authRepository: authRepository,
+        );
+
+        // AsyncValue.guard will capture both the exception and the stack trace when creating the output AsyncError state.
+        // But on our test we can't create an expected value with a matching stack trace, so we need to test with predicates.
+        // This allows us to write fine-grained expectations by checking if we have an error without worrying about the properties we don't care about (such as the stack trace in this case).
+        expectLater(
+          controller.stream,
+          emitsInOrder([
+            const AsyncLoading<void>(),
+            predicate<AsyncValue<void>>((value) {
+              expect(value.hasError, true);
+              return true;
+            }),
+          ]),
+        );
+        // run
+        await controller.signOut();
+        // verify
+        verify(authRepository.signOut).called(1);
+      },
+      timeout: const Timeout(Duration(milliseconds: 500)),
+    );
   });
 }
