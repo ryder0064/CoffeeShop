b8095fd61b4baf87be4223f73d13334f7f36568a
ryder.chen <ryder.chen@myviewboard.com>
2024-03-25 14:39:06 +0800

    Add the cart's data layer

---
+++ lib/src/features/cart/data/local/fake_local_cart_repository.dart
@@ -0,0 +1,24 @@
+import 'package:coffee_shop/src/features/cart/data/local/local_cart_repository.dart';
+import 'package:coffee_shop/src/features/cart/domain/cart.dart';
+import 'package:coffee_shop/src/utils/delay.dart';
+import 'package:coffee_shop/src/utils/in_memory_store.dart';
+
+class FakeLocalCartRepository implements LocalCartRepository {
+  FakeLocalCartRepository({this.addDelay = true});
+
+  final bool addDelay;
+
+  final _cart = InMemoryStore<Cart>(const Cart());
+
+  @override
+  Future<Cart> fetchCart() => Future.value(_cart.value);
+
+  @override
+  Stream<Cart> watchCart() => _cart.stream;
+
+  @override
+  Future<void> setCart(Cart cart) async {
+    await delay(addDelay);
+    _cart.value = cart;
+  }
+}
