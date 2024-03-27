316e1229bcb01ac5def243c75299446f8a8935b4
ryder.chen <ryder.chen@myviewboard.com>
2024-03-27 16:52:51 +0800

    Implement methods in the SembastCartRepository

--- lib/src/features/cart/domain/cart.dart
+++ lib/src/features/cart/domain/cart.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:coffee_shop/src/features/cart/domain/item.dart';
 import 'package:coffee_shop/src/features/products/domain/product.dart';
+import 'package:flutter/foundation.dart';
 
 /// Model class representing the shopping cart contents.
 class Cart {
@@ -9,6 +12,35 @@ class Cart {
   /// - key: product ID
   /// - value: quantity
   final Map<ProductID, int> items;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'items': items,
+    };
+  }
+
+  factory Cart.fromMap(Map<String, dynamic> map) {
+    return Cart(
+      Map<ProductID, int>.from(map['items']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Cart.fromJson(String source) => Cart.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Cart(items: $items)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Cart && mapEquals(other.items, items);
+  }
+
+  @override
+  int get hashCode => items.hashCode;
 }
 
 extension CartItems on Cart {
